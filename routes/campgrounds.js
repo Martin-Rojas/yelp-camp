d05929dd5537f248573eb4245da504e45d9d5369
MartinRojas <rojas.martin25@yahoo.com>
2023-05-08 19:09:34 -0700

    Restructure campgrounds, reviews and users Routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -7,30 +7,31 @@ const ExpressError = require("../utils/expressErrors");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 const campground = require("../controllers/campgrounds");
 
-// Show all campgrounds
-router.get("/", catchAsync(campground.index));
+router
+   .route(`/`)
+   // Show all campgrounds
+   .get(catchAsync(campground.index))
+   // Create a new campground
+   .post(
+      isLoggedIn,
+      validateCampground,
+      catchAsync(campground.createCampground)
+   );
 
 // show form to create a new campground
 router.get("/new", isLoggedIn, campground.newCampgroundForm);
 
-// Create a new campground
-router.post(
-   "/",
-   isLoggedIn,
-   validateCampground,
-   catchAsync(campground.createCampground)
-);
-
 // Edit campground
 router.get("/:id/edit", isAuthor, catchAsync(campground.editCampground));
 
-// Update a campground
-router.put("/:id", isAuthor, catchAsync(campground.updateCampground));
-
-// Show a campground
-router.get("/:id", catchAsync(campground.showCampground));
-
-// Delete a campground
-router.delete("/:id", isAuthor, catchAsync(campground.deleteCampground));
+router
+   // All have same path
+   .route(`/:id`)
+   // Show a campground
+   .get(catchAsync(campground.showCampground))
+   // Update a campground
+   .put(isAuthor, catchAsync(campground.updateCampground))
+   // Delete a campground
+   .delete(isAuthor, catchAsync(campground.deleteCampground));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,26 +7,27 @@ const { checkReturnTo } = require("../middleware");
 const users = require(`../controllers/users`);
 const user = require("../models/user");
 
-// render the register form
-router.get("/register", users.renderRegisterForm);
+router
+   .route(`/register`)
+   // render the register form
+   .get(users.renderRegisterForm)
+   // The register form is handle by this route. It register a user.
+   .post(catchAsync(users.registerUser));
 
-// The register form is handle by this route. It register a user.
-router.post("/register", catchAsync(users.registerUser));
-
-// This route will render the login form.
-router.get("/login", users.renderLoginRender);
-
-// This will hanlde the login submittion
-// "passport.authenticate" it will compare the hashpassword and the password the user entered.
-router.post(
-   "/login",
-   checkReturnTo,
-   passport.authenticate("local", {
-      failureFlash: true,
-      failureRedirect: "/login",
-   }),
-   users.authenticateUser
-);
+router
+   .route(`/login`)
+   // This route will render the login form.
+   .get(users.renderLoginRender)
+   // This will hanlde the login submittion
+   // "passport.authenticate" it will compare the hashpassword and the password the user entered.
+   .post(
+      checkReturnTo,
+      passport.authenticate("local", {
+         failureFlash: true,
+         failureRedirect: "/login",
+      }),
+      users.authenticateUser
+   );
 
 // Route to logout a user
 router.get("/logout", users.logout);
