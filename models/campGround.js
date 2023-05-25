06c99f00eeeeb9b5b3f2e379122adaeebf0055f0
MartinRojas <rojas.martin25@yahoo.com>
2023-05-24 19:57:19 -0700

    Adding a thumbnail vitual property

--- models/campGround.js
+++ models/campGround.js
@@ -3,17 +3,24 @@ const { campgroundSchema } = require("../schemas/schemas");
 const Review = require(`./review`);
 const Schema = mongoose.Schema;
 
+//https://res.cloudinary.com/drzfeh8fk/image/upload/w_200/v1684458171/YelpCamp/zrjwhnuyf58cpbjffs3d.jpg
+
+const ImageSchema = new Schema({ url: String, filename: String });
+
+ImageSchema.virtual(`thumbnail`).get(function () {
+   return this.url.replace(`/upload`, `/upload/w_200`);
+});
+
+ImageSchema.virtual(`thumbnail2`).get(function () {
+   return this.url.replace(`/upload`, `/upload/w_400`);
+});
+
 const CampgroundSchema = new Schema({
    title: String,
    price: Number,
    description: String,
    location: String,
-   images: [
-      {
-         url: String,
-         filename: String,
-      },
-   ],
+   images: [ImageSchema],
    author: { type: Schema.Types.ObjectId, ref: "User" },
    // this will make a realtionship with the review schema(one way relation)
    reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
